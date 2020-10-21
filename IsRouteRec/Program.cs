cae1a8cf3faed092eaa5a49e871ab5a7efcc7940
Stingray <jhompesc@avans.nl>
2020-10-21 16:05:11 +0200

    Recursion Challange
    Have fun ;)

---
+++ IsRouteRec/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsRouteRec
+{
+    class Program
+    {
+        
+        //signature
+        public static bool isRoute(int xStart, int yStart, int xEnd, int yEnd)
+        {
+
+            return false;
+        }
+
+        static void Main(string[] args)
+        {
+            bool a = isRoute(2, 10, 26, 12);
+
+            if (a == true)
+            {
+                Console.WriteLine("Route is valid");
+            }
+            else
+            {
+                Console.WriteLine("Route is not valid");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
